7649e6a92e42991b31441ae6496e65e8a4b4dbbd
binhex <megalith01@gmail.com>
2018-01-19 16:31:52 +0000

    switch to generic name for logger arg

--- lib/moviegrabber/mg_imdb_imdbpie.py
+++ lib/moviegrabber/mg_imdb_imdbpie.py
@@ -6,7 +6,7 @@ from itertools import islice
 
 # in, moviegrabber logger instance, imdb tt id
 # out, list of dicts with imdb metadata
-def imdb_json_api(mg_log, imdb_tt_id):
+def imdb_json_api(logger_instance, imdb_tt_id):
 
     # create imdb empty list, used to append dicts
     imdb_output_list = []
@@ -18,7 +18,7 @@ def imdb_json_api(mg_log, imdb_tt_id):
     try:
         get_title = imdb_instance.get_title(imdb_tt_id)
     except imdbpie.exceptions.ImdbAPIError:
-        mg_log.warning(u"failed to download json from imdb api")
+        logger_instance.warning(u"failed to download json from imdb api")
         return 1
 
     try:
@@ -26,34 +26,34 @@ def imdb_json_api(mg_log, imdb_tt_id):
         imdb_poster_url_dict = {'imdb_poster_url': imdb_poster_url}
         imdb_output_list.append(imdb_poster_url_dict)
     except KeyError:
-        mg_log.warning(u"failed to download json for imdb poster url")
+        logger_instance.warning(u"failed to download json for imdb poster url")
 
     try:
         imdb_title = get_title['base']['title']
         imdb_title_dict = {'imdb_title': imdb_title}
         imdb_output_list.append(imdb_title_dict)
     except KeyError:
-        mg_log.warning(u"failed to download json for imdb title")
+        logger_instance.warning(u"failed to download json for imdb title")
 
     try:
         imdb_year = get_title['base']['year']
         imdb_year_dict = {'imdb_year': imdb_year}
         imdb_output_list.append(imdb_year_dict)
     except KeyError:
-        mg_log.warning(u"failed to download json for imdb year")
+        logger_instance.warning(u"failed to download json for imdb year")
 
     try:
         imdb_runtime = get_title['base']['runningTimeInMinutes']
         imdb_runtime_dict = {'imdb_runtime': imdb_runtime}
         imdb_output_list.append(imdb_runtime_dict)
     except KeyError:
-        mg_log.warning(u"failed to download json for imdb runtime")
+        logger_instance.warning(u"failed to download json for imdb runtime")
 
     # Grab metadata from IMDb JSON for plot
     try:
         get_title_plot = imdb_instance.get_title_plot(imdb_tt_id)
     except imdbpie.exceptions.ImdbAPIError:
-        mg_log.warning(u"failed to download json for title plot")
+        logger_instance.warning(u"failed to download json for title plot")
         return 1
 
     try:
@@ -61,13 +61,13 @@ def imdb_json_api(mg_log, imdb_tt_id):
         imdb_plot_dict = {'imdb_plot': imdb_plot}
         imdb_output_list.append(imdb_plot_dict)
     except KeyError:
-        mg_log.warning(u"failed to download json for imdb plot")
+        logger_instance.warning(u"failed to download json for imdb plot")
 
     # Grab metadata from IMDb JSON for genres
     try:
         get_title_genres = imdb_instance.get_title_genres(imdb_tt_id)
     except imdbpie.exceptions.ImdbAPIError:
-        mg_log.warning(u"failed to download json for imdb title genres")
+        logger_instance.warning(u"failed to download json for imdb title genres")
         return 1
 
     try:
@@ -80,13 +80,13 @@ def imdb_json_api(mg_log, imdb_tt_id):
         imdb_genres_dict = {'imdb_genres': imdb_genres_list}
         imdb_output_list.append(imdb_genres_dict)
     except KeyError:
-        mg_log.warning(u"failed to download json for imdb genres")
+        logger_instance.warning(u"failed to download json for imdb genres")
 
     # Grab metadata from IMDb JSON for ratings
     try:
         get_title_ratings = imdb_instance.get_title_ratings(imdb_tt_id)
     except imdbpie.exceptions.ImdbAPIError:
-        mg_log.warning(u"failed to download json for imdb title ratings")
+        logger_instance.warning(u"failed to download json for imdb title ratings")
         return 1
 
     try:
@@ -94,20 +94,20 @@ def imdb_json_api(mg_log, imdb_tt_id):
         imdb_rating_dict = {'imdb_rating': imdb_rating}
         imdb_output_list.append(imdb_rating_dict)
     except KeyError:
-        mg_log.warning(u"failed to download json for imdb ratings")
+        logger_instance.warning(u"failed to download json for imdb ratings")
 
     try:
         imdb_rating_count = get_title_ratings['ratingCount']
         imdb_rating_count_dict = {'imdb_rating_count': imdb_rating_count}
         imdb_output_list.append(imdb_rating_count_dict)
     except KeyError:
-        mg_log.warning(u"failed to download json for imdb ratings count")
+        logger_instance.warning(u"failed to download json for imdb ratings count")
 
     # Grab metadata from IMDb JSON for writers, directors, characters and actors
     try:
         get_title_credits = imdb_instance.get_title_credits(imdb_tt_id)
     except imdbpie.exceptions.ImdbAPIError:
-        mg_log.warning(u"failed to download json for imdb title credits")
+        logger_instance.warning(u"failed to download json for imdb title credits")
         return 1
 
     try:
@@ -121,7 +121,7 @@ def imdb_json_api(mg_log, imdb_tt_id):
         imdb_output_list.append(imdb_writers_dict)
 
     except KeyError:
-        mg_log.warning(u"failed to download json for imdb writers")
+        logger_instance.warning(u"failed to download json for imdb writers")
 
     try:
 
@@ -135,7 +135,7 @@ def imdb_json_api(mg_log, imdb_tt_id):
         imdb_output_list.append(imdb_directors_dict)
 
     except KeyError:
-        mg_log.warning(u"failed to download json for imdb directors")
+        logger_instance.warning(u"failed to download json for imdb directors")
 
     try:
         imdb_cast = get_title_credits['credits']['cast']
@@ -158,6 +158,6 @@ def imdb_json_api(mg_log, imdb_tt_id):
         imdb_output_list.append(imdb_character_dict)
 
     except KeyError:
-        mg_log.warning(u"failed to download json for imdb characters")
+        logger_instance.warning(u"failed to download json for imdb characters")
 
     return imdb_output_list
